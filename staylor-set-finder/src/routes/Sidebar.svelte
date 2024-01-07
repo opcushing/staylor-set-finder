b467bfc5008274edd016e9c16af69eb76c1fa826
Owen Cushing <opcushing@gmail.com>
2024-01-06 18:56:45 -0600

    started collapsable sidebar

--- staylor-set-finder/src/routes/Sidebar.svelte
+++ staylor-set-finder/src/routes/Sidebar.svelte
@@ -1,17 +1,52 @@
 <script>
+	import Menu from 'svelte-material-icons/Menu.svelte';
+	import { fly, slide } from 'svelte/transition';
+
 	import ChordsDropdown from './Sidebar/ChordsDropdown.svelte';
+
+	let open = true;
+
+	function ToggleSidebar() {
+		open = !open;
+	}
 </script>
 
-<div class="content">
-	<h1>List of Pitch Class Sets</h1>
+<div class="sidebar">
+	{#if open}
+		<div class="content">
+			<h1>List of Pitch Class Sets</h1>
+			<ChordsDropdown />
+		</div>
+	{/if}
 	<!-- TODO: insert a clickable "MdMenu" icon. Collapses / uncollapses the menu on click. -->
-	<ChordsDropdown />
+	<button class="toggle" on:click={ToggleSidebar}>
+		<Menu size="2rem" />
+	</button>
 </div>
 
 <style>
-	.content {
-		width: 50%;
-		text-align: center;
+	h1 {
+		margin: 0;
+		text-wrap: nowrap;
+	}
+
+	.sidebar {
+		width: fit-content;
 		background-color: lightgrey;
+		display: flex;
+		flex-direction: row;
+	}
+
+	.content {
+		padding: 0.67em;
+		gap: 1rem;
+	}
+
+	.toggle {
+		align-self: flex-start;
+		margin-top: 1em;
+		cursor: pointer;
+		border: none;
+		background: none;
 	}
 </style>
